b4af3dadc1ffdc04218aa4e316bfeca355411568
KrishnaChaitanya1027 <kpotharaju8582@conestogac.on.ca>
2022-02-10 18:55:04 -0500

    Assignment 2 Payment Gateway API update

--- assignment1Dosa.js
+++ assignment1Dosa.js
@@ -10,12 +10,13 @@ const OrderState = Object.freeze({
     SIZE2: Symbol("size2"),
     TOPPINGS2: Symbol("toppings2"),
     DRINKS:  Symbol("drinks"),
-    BIRIYANI: Symbol("biriyani")
+    BIRIYANI: Symbol("biriyani"),
+    PAYMENT: Symbol("payment")
 });
 
-module.exports = class TacoOrder extends Order{
-    constructor(){
-        super();
+module.exports = class DosaOrder extends Order{
+    constructor(sNumber, sUrl){
+        super(sNumber, sUrl);
         this.stateCur = OrderState.WELCOME;
         this.item1 = ""; //menu has 2 items. will validate which is ordered based on user input
         this.size1 = "";
@@ -39,7 +40,7 @@ module.exports = class TacoOrder extends Order{
         let addSomething = 'Would you like to add a something else ? \n 1. Yes (Enter y)\n 2. No (Enter n)';
         let secondItemMsg = 'What second item from the menu would you like? \n 1. Dosa \n 2. Dabeli';
         let biriyaniMsg = 'Would you like to have Biriyani?';
-        let biriyaniProductMsg = 'Would you like Biriyani with that? \n 1. Veg Biriyani - $2.00 \n 2.  Chicken Biriyani - $2.00';
+        let biriyaniProductMsg = 'Would you like Biriyani with that? \n 1. Veg - $2.00 \n 2.  Chicken - $2.00 \n 3. No (Enter n)';
         let drinkMsg = "Would you like a drink with that?";
         switch(this.stateCur){
             case OrderState.WELCOME:
@@ -99,7 +100,7 @@ module.exports = class TacoOrder extends Order{
                 }
                 if (sInput.toLowerCase() == "n"){
                     this.stateCur = OrderState.BIRIYANI; 
-                    aReturn.push(biriyaniMsg); 
+                    aReturn.push(biriyaniProductMsg); 
                 }
                 break;
             case OrderState.ITEM2:
@@ -150,12 +151,21 @@ module.exports = class TacoOrder extends Order{
             case OrderState.BIRIYANI:
                 this.stateCur = OrderState.DRINKS;
                 debugger;
-                if(sInput.toLowerCase() == "y" || sInput.toLowerCase()=='yes'){ 
+                 this.biriyani = 'n';
+                if(sInput.toLowerCase() == 'veg' || sInput.toLowerCase() == 'chicken' ){ 
                     this.biriyani = sInput;
+                } 
+                else if(sInput.toLowerCase() != 'n' && sInput.toLowerCase() != 'no'){
+                    aReturn.push(validItem) 
+                    aReturn.push(biriyaniProductMsg); 
+                    this.stateCur = OrderState.BIRIYANI;
+                    break;
                 }
                 aReturn.push(drinkMsg );
                 break;
             case OrderState.DRINKS:
+                this.stateCur = OrderState.PAYMENT;
+               
                 this.isDone(true);
                 if(sInput.toLowerCase()== "y" || sInput.toLowerCase()=='yes'){
                     this.drinks = sInput;
@@ -172,7 +182,7 @@ module.exports = class TacoOrder extends Order{
                 let biriyaniCost = 2.00;
                 let sDrinksCost = 2.50;
 
-                if(this.biriyani){
+                if(this.biriyani != 'n' && this.biriyani != 'no'){
                     beforePrice += biriyaniCost;
                 }
                 if(this.drinks){ 
@@ -224,25 +234,100 @@ module.exports = class TacoOrder extends Order{
                 }
                 let tax = 0.13;
                 this.total = tax * beforePrice + beforePrice; 
-
+                this.nOrder =  this.total ;
                 aReturn.push("Thank-you for your order of");
-                aReturn.push(`${this.size1 == 's' ? 'small' : this.size1 == 'm' ? 'medium' : 'large'} ${this.item1}  ${(this.toppings1 != 'n' && this.toppings1 != 'no') ? 'with' + this.toppings1 : ''}`);
+                aReturn.push(`${this.size1 == 's' ? 'small' : this.size1 == 'm' ? 'medium' : 'large'} ${this.item1}  ${(this.toppings1 != 'n' && this.toppings1 != 'no') ? 'with ' + this.toppings1 : ''}`);
                 if(this.item2){ 
-                aReturn.push(`${this.size1 == 's' ? 'small' : this.size1 == 'm' ? 'medium' : 'large'} ${this.item2} ${(this.toppings2 != 'n' && this.toppings2 != 'no') ? 'with' + this.toppings2 : ''}`);
+                aReturn.push(`${this.size1 == 's' ? 'small' : this.size1 == 'm' ? 'medium' : 'large'} ${this.item2} ${(this.toppings2 != 'n' && this.toppings2 != 'no') ? 'with ' + this.toppings2 : ''}`);
                 }
-                debugger;
-                if(this.biriyani){
-                    aReturn.push(this.biriyani);
+                console.log('this.biriyani');
+                if(this.biriyani != 'n' && this.biriyani != 'no'){
+                    aReturn.push(this.biriyani + 'Biriyani');
                 }
                 if(this.drinks){
                     aReturn.push(this.drinks);
                 }
                 let d = new Date(); 
                 d.setMinutes(d.getMinutes() + 20);
-               
-                aReturn.push(`Please pick it up at ${d.toDateString() + ' ' + d.toLocaleTimeString()}. Your order total is $${this.total.toFixed(2)}`); 
+                aReturn.push(`Please pay for your order here`);
+                aReturn.push(`${this.sUrl}/payment/${this.sNumber}/`);
+                //aReturn.push(`Please pick it up at ${d.toDateString() + ' ' + d.toLocaleTimeString()}. Your order total is $${this.total.toFixed(2)}`); 
                 break;
-        }
+            
+             case OrderState.PAYMENT:
+                 debugger;
+                    //console.log(sInput);
+                    this.isDone(true);
+                    let purDate = new Date();
+                    purDate.setMinutes(purDate.getMinutes() + 20);
+                  
+                    let shippingdata = JSON.stringify(sInput.purchase_units[0].shipping);
+                  let shipping = JSON.parse(shippingdata);
+                  let address = shipping.address;
+                    aReturn.push(`Your order will be delivered to ${address.address_line_1} ${address.admin_area_2} ${address.admin_area_1} ${address.postal_code} ${address.country_code} at  ${purDate.toTimeString()}`);
+                    break;
+
+
+            }
         return aReturn;
     }
+
+    renderForm(sTitle = "-1", sAmount = "-1"){
+        debugger;
+        // your client id should be kept private
+        if(sTitle != "-1"){
+          this.sItem = sTitle;
+        }
+        if(sAmount != "-1"){
+          this.nOrder = sAmount;
+        }
+        const sClientID = process.env.SB_CLIENT_ID || 'put your client id here for testing ... Make sure that you delete it before committing'
+        return(`
+        <!DOCTYPE html>
+    
+        <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Ensures optimal rendering on mobile devices. -->
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" /> <!-- Optimal Internet Explorer compatibility -->
+        </head>
+        
+        <body>
+          <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+          <script
+            src="https://www.paypal.com/sdk/js?client-id=${sClientID}"> // Required. Replace SB_CLIENT_ID with your sandbox client ID.
+          </script>
+          Thank you ${this.sNumber} for your ${this.item1}  ${this.item2 != '' ? ' & ' + this.item2 : ''}   order of $${this.nOrder}.
+          <div id="paypal-button-container"></div>
+    
+          <script>
+            paypal.Buttons({
+                createOrder: function(data, actions) {
+                  // This function sets up the details of the transaction, including the amount and line item details.
+                  return actions.order.create({
+                    purchase_units: [{
+                      amount: {
+                        value: '${this.nOrder}'
+                      }
+                    }]
+                  });
+                },
+                onApprove: function(data, actions) {
+                  // This function captures the funds from the transaction.
+                  return actions.order.capture().then(function(details) {
+                    // This function shows a transaction success message to your buyer.
+                    $.post(".", details, ()=>{
+                      window.open("", "_self");
+                      window.close(); 
+                    });
+                  });
+                }
+            
+              }).render('#paypal-button-container');
+            // This function displays Smart Payment Buttons on your web page.
+          </script>
+        
+        </body>
+            
+        `);
+    
+      }
 }
